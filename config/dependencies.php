50cdac0a95ca1688809f4b9403b6f05f65fabb0f
Asanka Jayashan <info.encodeunlimited@gmail.com>
2026-08-21 15:16:38 +0530

    feat: implement PHP-DI container definitions with SQLite/MySQL database support and service wiring

--- config/dependencies.php
+++ config/dependencies.php
@@ -182,7 +182,8 @@ return function (ContainerBuilder $containerBuilder) {
                 $c->get(\App\Repositories\AppointmentRepository::class),
                 $c->get(\App\Repositories\CustomerRepository::class),
                 $c->get(\App\Services\LoyaltyService::class),
-                $c->get(\App\Repositories\PackageRepository::class)
+                $c->get(\App\Repositories\PackageRepository::class),
+                $c->get(\App\Repositories\CustomerPackageRepository::class)
             );
         },
     ]);

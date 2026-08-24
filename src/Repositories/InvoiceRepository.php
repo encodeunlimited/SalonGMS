17b93bca7875d6d1a85fc507318c88ea1e4a7a3f
encodeunlimited <118004281+encodeunlimited@users.noreply.github.com>
2026-08-24 14:42:32 +0530

    feat: add InvoiceRepository to handle invoice database operations and queries

--- src/Repositories/InvoiceRepository.php
+++ src/Repositories/InvoiceRepository.php
@@ -71,8 +71,8 @@ class InvoiceRepository extends BaseRepository
     {
         $stmt = $this->db->prepare("
             SELECT i.*, c.name as customer_name, c.email as customer_email, c.phone as customer_phone,
-                   (SELECT GROUP_CONCAT(DISTINCT a.stylist_name SEPARATOR ', ') FROM appointments a WHERE a.invoice_id = i.id OR a.id = i.appointment_id) as stylist_name,
-                   (SELECT GROUP_CONCAT(ii.description SEPARATOR ', ') FROM invoice_items ii WHERE ii.invoice_id = i.id) as service_names
+                   (SELECT GROUP_CONCAT(DISTINCT a.stylist_name, ', ') FROM appointments a WHERE a.invoice_id = i.id OR a.id = i.appointment_id) as stylist_name,
+                   (SELECT GROUP_CONCAT(ii.description, ', ') FROM invoice_items ii WHERE ii.invoice_id = i.id) as service_names
             FROM {$this->table} i
             LEFT JOIN customers c ON i.customer_id = c.id
             WHERE i.id = :id LIMIT 1
